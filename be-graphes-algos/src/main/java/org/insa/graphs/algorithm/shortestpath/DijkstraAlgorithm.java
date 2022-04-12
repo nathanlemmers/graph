871469f39d2ebaa84b10ba7a6bce4bcaf0a86522
Lemmers Nathan <lemmers@insa-20708>
2022-04-12 18:14:32 +0200

    Fin du Dijkstra

--- be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
+++ be-graphes-algos/src/main/java/org/insa/graphs/algorithm/shortestpath/DijkstraAlgorithm.java
@@ -1,16 +1,17 @@
 package org.insa.graphs.algorithm.shortestpath;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 import org.insa.graphs.algorithm.AbstractSolution.Status;
+import org.insa.graphs.algorithm.utils.BinaryHeap;
 import org.insa.graphs.model.Arc;
 import org.insa.graphs.model.Graph;
 import org.insa.graphs.model.Node;
 import org.insa.graphs.model.Path;
 import org.insa.graphs.model.Label;
-
 
 public class DijkstraAlgorithm extends ShortestPathAlgorithm {
 
@@ -21,51 +22,62 @@ public class DijkstraAlgorithm extends ShortestPathAlgorithm {
     @Override
     protected ShortestPathSolution doRun() {
         final ShortestPathData data = getInputData();
+        BinaryHeap<Label> tas = new BinaryHeap<Label>();
+        List<Arc> chemin = new ArrayList<Arc>();
         ShortestPathSolution solution = null;
         Graph graph = data.getGraph();
         final int nbNodes = graph.size();
         List<Node> tabNode = graph.getNodes();
-        // TODO:
-        List<Label> tabLabel = new ArrayList<Label>() ;
-        for (int i=0; i<nbNodes; i++){
-            tabLabel.add(new Label(i, false, Double.POSITIVE_INFINITY, null));
+        Node fin = data.getDestination();
+        List<Label> tabLabel = new ArrayList<Label>();
+        Node depart = data.getOrigin();
+        for (int i = 0; i < nbNodes; i++) {
+            if (tabNode.get(i).compareTo(depart) == 0) {
+                tabLabel.add(new Label(i, true, 0, null));
+                tas.insert(tabLabel.get(i));
+            } else {
+                tabLabel.add(new Label(i, false, Float.POSITIVE_INFINITY, null));
+            }
         }
-        boolean found=false ;
-        Node depart = data.getOrigin() ;
-        while (!found){
-            for (int i=0; i<nbNodes; i++){
-                for (int j=0; j<depart.getSuccessors().size();j++){
-                    if (depart.getSuccessors().get(j).getDestination().compareTo(tabNode.get(i))==1) {
-                        if (!tabLabel.get(i).getMarque()){
-                            if (tabLabel.get(i).getCost()>tabLabel.get(depart.getId()).getCost() + depart.getSuccessors().get(j).getMinimumTravelTime()){
-                                tabLabel.get(i).setCost(tabLabel.get(depart.getId()).getCost() + depart.getSuccessors().get(j).getMinimumTravelTime()) ;
-                                tabLabel.get(i).setPere(depart.getSuccessors().get(j)) ;
-                            }
+        notifyOriginProcessed(depart);
+        Label Labeldest = tabLabel.get(tabNode.indexOf(fin));
+        Label min;
+        int index;
+        Label successeur;
+        Arc a;
+        while (!Labeldest.getMarque() && !tas.isEmpty()) {
+            min = tas.findMin();
+            index = tabLabel.indexOf(min);
+            tabLabel.get(index).setMarque(true);
+            notifyNodeMarked(tabNode.get(tabLabel.indexOf(min)));
+            for (int i = 0; i < tabNode.get(index).getSuccessors().size(); i++) {
+                successeur = tabLabel.get(tabNode.indexOf(tabNode.get(index).getSuccessors().get(i).getDestination()));
+                a = tabNode.get(index).getSuccessors().get(i);
+                if (!successeur.getMarque() && data.isAllowed(a)) {
+                    if (successeur.getCost() > data.getCost(a) + min.getCost()) {
+                        if (Float.isInfinite(successeur.getCost())) {
+                            tas.insert(successeur);
+                            notifyNodeMarked(a.getDestination());
                         }
+                        successeur.setCost((float) data.getCost(a) + min.getCost());
+                        successeur.setPere(a);
+
                     }
                 }
-
-//La prochaine fois : Là on a changé les valeurs en partant de départ pour tous les labels si on avait une distance plus petite. Maintenant, tu dois comparer tous les labels, et le plus petit doit devenir depart et sa marque doit devenir vraie.
-
-
-
             }
-
-
-
-
-
+            tas.remove(min);
         }
-
-
-
-
-
-
-
-
-
-
+        notifyDestinationReached(fin);
+        while (Labeldest.getPere() != null) {
+            chemin.add(Labeldest.getPere());
+            Labeldest = tabLabel.get(tabNode.indexOf(Labeldest.getPere().getOrigin()));
+        }
+        Collections.reverse(chemin);
+        if (tabLabel.get(fin.getId()).getMarque()) {
+            solution = new ShortestPathSolution(data, Status.OPTIMAL, new Path(graph, chemin));
+        } else {
+            solution = new ShortestPathSolution(data, Status.INFEASIBLE);
+        }
         return solution;
     }
 
--- be-graphes-model/src/main/java/org/insa/graphs/model/Label.java
+++ be-graphes-model/src/main/java/org/insa/graphs/model/Label.java
@@ -1,39 +1,60 @@
 package org.insa.graphs.model;
 
+public class Label implements Comparable<Label> {
 
-public class Label {
+    private int id;
+    private boolean ok;
+    private float cost;
+    private Arc arc;
 
-private int id ;
-private boolean ok ;
-private double cost ;
-private Arc arc ;
-
-
-    public Label(int som, boolean marque, double cout, Arc pere) {
-        this.id=som ;
-        this.ok=marque ;
-        this.cost=cout;
-        this.arc=pere ;
+    public Label(int som, boolean marque, float cout, Arc pere) {
+        this.id = som;
+        this.ok = marque;
+        this.cost = cout;
+        this.arc = pere;
     }
 
-    public double getCost(){
-        return this.cost ;
+    public int getId() {
+        return this.id;
     }
 
-    public boolean getMarque(){
-        return this.ok ;
+    public float getCost() {
+        return this.cost;
     }
 
-    public int associate(){
-        return this.id ;
+    public boolean getMarque() {
+        return this.ok;
     }
 
-    public void setCost(double cout){
-        this.cost=cout ;
+    public Arc getPere() {
+        return this.arc;
     }
 
-    public void setPere(Arc pere){
-        this.arc=pere ;
+    public int associate() {
+        return this.id;
+    }
+
+    public void setCost(float cout) {
+        this.cost = cout;
+    }
+
+    public void setPere(Arc pere) {
+        this.arc = pere;
+    }
+
+    public void setMarque(boolean b) {
+        this.ok = b;
+    }
+
+    public int compareTo(Label autre) {
+        float res = this.cost - autre.cost;
+        if (res < 0) {
+            return -1;
+        } else if (res == 0) {
+            return 0;
+        } else {
+            return 1;
+        }
     }
 
 }
